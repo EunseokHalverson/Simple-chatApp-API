1c8e8f783d59772ca64da434309837951ae2fe3e
Eunseok Halverson <es.halvers@gmail.com>
2017-12-09 20:35:08 +0900

    added slide bar

--- Simple-chatApp-API/Simple-chatApp-API/Controller/ChatVC.swift
+++ Simple-chatApp-API/Simple-chatApp-API/Controller/ChatVC.swift
@@ -10,16 +10,15 @@ import UIKit
 
 class ChatVC: UIViewController {
 
+    @IBOutlet weak var menuBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
 
     /*
