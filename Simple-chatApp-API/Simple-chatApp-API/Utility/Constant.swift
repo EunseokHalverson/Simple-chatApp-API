306fced7eaf6bfa0966ab7d28c236893b3a452e0
Eunseok Halverson <es.halvers@gmail.com>
2017-12-10 18:34:41 +0900

    loginVC, channelVC updated

---
+++ Simple-chatApp-API/Simple-chatApp-API/Utility/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  Simple-chatApp-API
+//
+//  Created by Eunseok on 2017. 12. 10..
+//  Copyright © 2017년 Eunseok. All rights reserved.
+//
+
+import Foundation
+
+
+let TO_LOGIN = "toLogin"
---
+++ Simple-chatApp-API/Simple-chatApp-API/Utility/File.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  Simple-chatApp-API
+//
+//  Created by Eunseok on 2017. 12. 10..
+//  Copyright © 2017년 Eunseok. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
